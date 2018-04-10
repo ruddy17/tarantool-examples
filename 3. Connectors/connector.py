5b1630b9124b94ba15ff2ce382aa43175e2a1b71
Sergey Omelchenko <>
2018-04-10 12:37:10 +0300

    no message

--- 3. Connectors/connector.py
+++ 3. Connectors/connector.py
@@ -1,6 +1,7 @@
 import tarantool
+from pprint import pprint
 
-connection = tarantool.connect(port=3300, user='guest')
+connection = tarantool.connect(host='127.0.0.1', port=3300, user='me', password='tsss')
 
 space = connection.space('test')
 
@@ -8,6 +9,8 @@ for i in range(10):
 	space.insert((None, i, 'hello'+str(i)))
 
 print(space.select())
+
+pprint(space.eval('return box.schema').data)
 
 connection.eval('box.space.test:create_index("value", {unique = false, parts = {2,"unsigned"}})')
 
--- 3. Connectors/init.lua
+++ 3. Connectors/init.lua
@@ -4,9 +4,10 @@ box.cfg{
 }
 
 box.once("schema", function()
-   box.schema.user.grant('guest', 'read,write,execute', 'universe')
-   box.schema.space.create("test")
-   box.schema.sequence.create('S')
-   box.space.test:create_index("primary", {sequence='S', parts = {1,'unsigned'}})
-   print('========= box.once executed =========')
+  box.schema.user.create('me', {password = 'tsss'})
+  box.schema.user.grant('me', 'read,write,execute', 'universe')
+  box.schema.space.create("test")
+  box.schema.sequence.create('S')
+  box.space.test:create_index("primary", {sequence='S', parts = {1,'unsigned'}})
+  print('========= box.once executed =========')
 end)
