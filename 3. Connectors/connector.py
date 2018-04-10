f080288d9d4b3049190d91fd910c0b6971f0fa21
Sergey Omelchenko <>
2018-04-10 09:48:14 +0300

    no message

---
+++ 3. Connectors/connector.py
@@ -0,0 +1,22 @@
+import tarantool
+
+con = tarantool.connect(port=3300, user='guest')
+
+space = con.space('test')
+
+for i in range(10):
+	space.insert((None, i, 'hello'+str(i)))
+
+print(space.select())
+
+con.eval('box.space.test:create_index("value", {unique = false, parts = {2,"unsigned"}})')
+
+print(space.select(index=1))
+
+space.update(2, [('=',2, 'bye')])
+
+space.upsert((25,1,'hi'), [('=', 2, 'bye')])
+space.upsert((1,1,'hi'), [('=', 2, 'bye')])
+
+space.delete(1)
+space.replace((3,111,'changed'))
