6dfba6aaf1095525b7942c8678491fe18ea3e720
Sergey Omelchenko <>
2018-04-10 09:08:50 +0300

    -

---
+++ 3. Connectors/init.lua
@@ -0,0 +1,12 @@
+-- instance file for any of the two masters
+box.cfg{
+  listen      = 3300
+}
+
+box.once("schema", function()
+   box.schema.user.grant('guest', 'read,write,execute', 'universe')
+   box.schema.space.create("test")
+   box.schema.sequence.create('S')
+   box.space.test:create_index("primary", {sequence='S', parts = {1,'unsigned'}})
+   print('========= box.once executed =========')
+end)
--- 6. Replication/master2/master2.lua
+++ 6. Replication/master2/master2.lua
@@ -1,19 +1,26 @@
--- instance file for any of the two masters
-replica = 2
+local uuid = require('uuid')
+
 box.cfg{
   listen      = 3302,
   replication = {'replicator:password@127.0.0.1:3301',  -- master1 URI
                  'replicator:password@127.0.0.1:3302'}, -- master2 URI
 
-  read_only   = false
+  read_only   = false,
+  hot_standby = true
 }
+
+function insert(...)
+  box.space.test:insert({uuid.str(), ...})
+end
+
 box.once("schema", function()
    box.schema.user.create('replicator', {password = 'password'})
+   box.schema.user.grant('guest', 'read,write,execute', 'universe')
    box.schema.user.grant('replicator', 'replication') -- grant replication role
    box.schema.space.create("test")
-   box.schema.sequence.create('S')
-   box.space.test:create_index("primary", {sequence='S', parts = {1,'unsigned', 2, 'unsigned'}})
-   print('box.once executed on master #2')
+   -- box.schema.sequence.create('S')
+   -- box.space.test:create_index("primary", {sequence='S', parts = {1,'unsigned', 2, 'unsigned'}})
+   box.space.test:create_index("primary", {type = 'hash', parts = {1,'string'}})
+   print('box.once executed')
 end)
-local console = require('console')
-console.start()
+
